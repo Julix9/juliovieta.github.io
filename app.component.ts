764e0a8bcb7f862ccdb6178da882a517c565efb6
Julio Vieta <julioc.vieta@gmail.com>
2023-08-10 15:07:29 +0200

    index ruta larga

---
+++ app.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Calculator';
+  numero1:number=0;
+  numero2:number=0;
+  resultado:number=0;
+
+  sumar(){
+
+    this.resultado=this.numero1+this.numero2;
+  }
+
+  restar(){
+
+    this.resultado=this.numero1-this.numero2;
+  }
+  multiplicar(){
+
+    this.resultado=this.numero1*this.numero2;
+  }
+  dividir(){
+
+    this.resultado=this.numero1/this.numero2;
+  }
+}
